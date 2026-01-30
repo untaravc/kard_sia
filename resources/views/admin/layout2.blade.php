ff1ba16be10cd2930becfcbcd44bfa3024aa3245
Untara Vivi Chahya <vyvy1777@gmail.com>
2026-01-31 01:16:35 +0700

    laravel mix

--- resources/views/admin/layout2.blade.php
+++ resources/views/admin/layout2.blade.php
@@ -5,10 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>SIA Kardio</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
     <div id="app"></div>
-    <script src="/js/app.js"></script>
+    <script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
